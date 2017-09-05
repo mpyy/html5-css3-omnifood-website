741f39a73fa0eded93d0203c06ad31ba6c94d599
Matias Pyy <matias.pyy@gmail.com>
2017-09-05 19:36:29 +0300

    Scrolling to elements

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,19 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    /* No framework class syntax: */
+    /* For the sticky navigation */
 
+    /* No framework class syntax */
     var waypoint = new Waypoint({
-      element: $('.js--section-features'),
-      handler: function(direction) {
-        if (direction == "down") {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-      },
-      offset: '60px'
+        element: $('.js--section-features'),
+        handler: function (direction) {
+            if (direction === "down") {
+                $('nav').addClass('sticky');
+            } else {
+                $('nav').removeClass('sticky');
+            }
+        },
+        offset: '60px'
     });
-
 
     /* jQuery syntax */
     /*
@@ -27,5 +27,54 @@ $(document).ready(function() {
         offset: '60px'
     });
     */
+   
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+    });
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+    
+    /* Navigation scroll */
+    /* Smooth scrolling - https://css-tricks.com/snippets/jquery/smooth-scrolling/ */
+    
+    // Select all links with hashes
+    $('a[href*="#"]')
+      // Remove links that don't actually link to anything
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function(event) {
+        // On-page links
+        if (
+          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+          && 
+          location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          // Does a scroll target exist?
+          if (target.length) {
+            // Only prevent default if animation is actually gonna happen
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000, function() {
+              // Callback after animation
+              // Must change focus!
+              var $target = $(target);
+              $target.focus();
+              if ($target.is(":focus")) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              };
+            });
+          }
+        }
+      });
     
 });
